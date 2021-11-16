81bf0129419e2c1874c3a867e56d06966c97c050
Aichuk Tripura <living.being123@gmail.com>
2021-11-16 11:11:45 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer, DefaultTheme} from "@react-navigation/native"
+import {Home} from "./components"
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const myTheme = {
+  ...DefaultTheme,
+  colors : {
+    ...DefaultTheme.colors,
+    border : "transparent"
+  },
+}
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme ={myTheme}>
+      <Stack.Navigator initialRouteName = {"Home"}>
+        <Stack.Screen name="Home" component = {Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+import {icons, images, FONTS, COLORS, SIZES} from "../constants"
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
